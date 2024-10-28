229443fd6e11770f2f6105182839e5c488cf34c9
Ljojua15 <jojualasha15@gmail.com>
2024-10-28 09:05:23 +0400

    'test'

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -46,6 +46,8 @@ export class AppointmentsComponent implements OnInit {
   removeAppo(index: number) {
     this.appointments.splice(index, 1);
     this.appointments.forEach((appointment, idx) => {
+      console.log(appointment, idx);
+
       appointment.id = idx + 1;
     });
     localStorage.setItem('appo', JSON.stringify(this.appointments));
