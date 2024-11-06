a852084b09e33f08a983d4908806de6d86f9f639
Ljojua15 <jojualasha15@gmail.com>
2024-11-06 10:56:13 +0400

    'test'

---
+++ src/app/box-animation/box-animation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-animation',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-animation.component.html',
+  styleUrl: './box-animation.component.scss',
+})
+export class BoxAnimationComponent {}
